c3dce5c2b5be5738df1f49f958529f7bf2dd20a5
kawashima-mozart <nowhere.no.where.now.here.orz@gmail.com>
2020-12-24 14:09:14 +0900

    複数問作成

--- app.js
+++ app.js
@@ -1,32 +1,65 @@
-const question = 'ゲーム市場,最も売れたゲームは？'
-const answers = [
-  'スーパーファミコン',
-  'プレイステーション',
-  'ニンテンドースイッチ',
-  'ニンテンド-DS'
-];
-const correct = 'ニンテンド-DS';
+const quiz = [
+  {
+    question:'お酢に卵を殻ごといれると卵はどうなるでしょう？',
+    answers: [
+      '透明な卵になる',
+      '鏡のようになんでもうつる卵になる',
+      '卵が溶けてなくなる',
+      '卵が石のように堅くなる'
+    ],
+    correct: '透明な卵になる'
+  },{
+    question:'リンカーンは大統領になる前は何をしていたでしょうか？？',
+    answers: [
+      'プロ野球選手',
+      '猟師',
+      'レスラー',
+      'タクシー運転手'
+    ],
+    correct: 'レスラー'
+  },{
+    question:'飛行機の中で食べるように作られた野菜があります。その野菜はどれでしょう？',
+    answers: [
+      'ミニトマト',
+      'パプリカ',
+      'アボカド',
+      'ズッキーニ'
+    ],
+    correct: 'ミニトマト'
+  }
+]
+
+const quizLength = quiz.length
+let quizIndex = 0
+
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 
 //クイズ野問題文、選択肢
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex ++;
   }
 };
 setupQuiz();
 
 const clickHandler = (e) =>{
-  if (correct === e.target.textContent ){
+  if (quiz[quizIndex].correct === e.target.textContent ){
     window.alert('正解！');
   }else{
     window.alert('不正解！')
   }
+  quizIndex ++;
+
+  if (quizIndex < quizLength){
+    setupQuiz();
+    } else{
+      window.alert('終了')
+    }
 };
 
 let handlerIndex = 0;
